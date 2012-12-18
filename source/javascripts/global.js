62c2969c34587e5d034d4ff3ad238c55d6edeaa7
Mike Cravey <techie@craveytrain.com>
2012-12-17 22:47:21 -0600

    Forget IE (<9)

--- source/javascripts/global.js
+++ source/javascripts/global.js
@@ -1,56 +1,23 @@
 (function() {
-var classes = (function(){
-	var add = function( el, className, force ) {
-		if ( force || !has( el, className ) ) el.className += ' ' + className
-		},
-		has = function( el, className ) {
-			var re = RegExp( '\\b' + className + '\\b' )
-
-			return re.test(el.className)
-
-		},
-		remove = function( el, className, force ) {
-			var re
-
-			if ( force || has( el, className ) ) {
-				re = RegExp( '\\b' + className + '\\b' )
-
-				el.className = el.className.replace(re, '')
-			}
-		},
-		toggle = function( el, className ) {
-			if ( has( el, className ) ) {
-				remove( el, className, true )
-			} else {
-				add( el, className, true )
-			}
-		}
-
-	return {
-		add: add,
-		has: has,
-		remove: remove,
-		toggle: toggle
-	}
-}());
+'use strict';
 
 var addEvent = (function() {
 	var queue = {},
 		w3c = false,
 		ie = false,
 		add = function( el, event, fn ) {
-			if ( !el || !event || !fn ) return
+			if ( !el || !event || !fn ) return;
 
-			var eventQ = queue[ event ]
+			var eventQ = queue[ event ];
 
-			if (!eventQ) eventQ = queue[ event ] = {}
+			if (!eventQ) eventQ = queue[ event ] = {};
 
 			if ( !eventQ[ el ] ) {
 
-				eventQ[ el ] = [ fn ]
+				eventQ[ el ] = [ fn ];
 
 				if ( w3c ) {
-					el.addEventListener( event, run, false)
+					el.addEventListener( event, run, false);
 				} else if ( ie ) {
 					el.attachEvent( 'on' + event, run);
 				} else {
@@ -58,42 +25,42 @@ var addEvent = (function() {
 				}
 			// Already run before
 			} else {
-				eventQ[ el ].push( fn )
+				eventQ[ el ].push( fn );
 			}
 		},
 		run = function( event ) {
 			var eventQ = queue[ event.type ][ this ],
 				l = eventQ.length,
-				i = 0
+				i = 0;
 
 				for ( ; i < l; i++ ) {
-					eventQ[ i ].apply(this, [ event ])
+					eventQ[ i ].apply(this, [ event ]);
 				}
-		}
+		};
 
 		if ( document.addEventListener ) {
-			w3c = true
+			w3c = true;
 		} else if ( document.attachEvent ) {
-			ie = true
+			ie = true;
 		}
 
-	return add
+	return add;
 }());
 
 // nav fixer
 (function() {
 	var onScroll = function() {
 			if ( window.scrollY > 240 ) {
-				classes.add( navBar, 'fixed' )
+				navBar.classList.add('fixed');
 			} else {
-				classes.remove( navBar, 'fixed' )
+				navBar.classList.remove('fixed');
 			}
-		}
+		};
 
-	if ( classes.has( document.body, 'home' ) && document.documentElement.clientWidth > 800 ) {
-		var navBar = document.querySelector( '.page-nav' )
+	if ( document.body.classList.contains('home') && document.documentElement.clientWidth > 800 ) {
+		var navBar = document.querySelector( '.page-nav' );
 
-		addEvent( window, 'scroll', onScroll )
+		addEvent( window, 'scroll', onScroll );
 	}
 
 }());
